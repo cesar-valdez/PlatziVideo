04696a4a920a79d4ab92181d9656cdcf33c70162
César Valdez <cesar.valdez1095@gmail.com>
2020-01-31 02:49:43 -0600

    feat: add register view with a route

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Register from '../containers/Register';
 
 const App = () => (
   <Router>
     <Route exact path='/' component={Home} />
     <Route exact path='/login' component={Login} />
+    <Route exact path='/register' component={Register} />
   </Router>
 )
 
