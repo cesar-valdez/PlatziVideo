69ccd405e60f7cc2b66ace9942595b4834058b0e
César Valdez <cesar.valdez1095@gmail.com>
2020-02-01 15:44:35 -0600

    feat: add a layout for header, footer and a children

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,16 +4,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from '../views/Home';
 import Login from '../views/Login';
 import Register from '../views/Register';
+import Layout from '../components/Layout';
 import NotFound from '../views/NotFound';
 
 const App = () => (
   <Router>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </Router>
 );
 
