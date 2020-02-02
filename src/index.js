24100ce88cde4698e7ce47a1238dc27a4e1f1150
César Valdez <cesar.valdez1095@gmail.com>
2020-02-01 20:50:37 -0600

    feat: add redux and react-redux library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CreateStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
